a59d806a1d061024415a1fbdde44d3f3d8720c6b
Nicklasvsams <nicklasvsams@gmail.com>
2020-08-24 14:57:59 +0200

    ApiCreation

---
+++ XivApi/XivApi/ApiControllers/MyApiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XivApi.Models;
+using XivApi.Service;
+
+namespace XivApi.Website.ApiControllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MyApiController : ControllerBase
+    {
+        public XivDataService Service { get; }
+
+        public MyApiController(XivDataService service)
+        {
+            Service = service;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetMyApi()
+        {
+            var myApiWeapon = Service.GetXivWeapon();
+
+            return this.Ok(myApiWeapon);
+        }
+    }
+}
